0b5ac3dc39edb2e2cac94a53e5fd151af685d557
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-07 22:11:59 +0100

    Adding Styles to Navigation

--- src/views/LoginViews.tsx
+++ src/views/LoginViews.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 const LoginViews = () => {
       return (
             <>
-
-                  <nav>
-                        <Link to="/auth/register">
+                  <h1 className="text-4xl text-white font-bold"> Iniciar Sessión</h1>
+                  <nav className="mt-10">
+                        <Link
+                              className="text-center text-white text-lg block"
+                              to="/auth/register">
                               ¿No tienes cuenta? Crea una aqui
                         </Link>
                   </nav>
--- src/views/RegisterViews.tsx
+++ src/views/RegisterViews.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 const RegisterViews = () => {
       return (
             <>
-
-                  <nav>
-                        <Link to="/auth/login">
-                              ¿Ingresa al Login?  Aqui!!
+                  <nav className="mt-10">
+                        <Link
+                              className="text-center text-white text-lg block"
+                              to="/auth/login">
+                              ¿Ya tienes una cuenta?  Inicia Sessión
                         </Link>
                   </nav>
             </>
