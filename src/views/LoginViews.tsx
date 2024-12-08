0776bec270cc416b42c3948bd49fca331165b025
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-08 09:59:47 +0100

    Adding React Hook Form Events and Validation

--- src/views/LoginViews.tsx
+++ src/views/LoginViews.tsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom"
 
 const LoginViews = () => {
       const { register, watch, handleSubmit, formState: { errors } } = useForm();
+
+      const handleRegister = () => {
+            console.log('Desde handleRegister...');
+      }
+
       return (
             <>
                   <h1 className="text-4xl text-white font-bold"> Iniciar Sessión</h1>
 
                   <form
-                        onSubmit={() => { }}
+                        onSubmit={handleSubmit(handleRegister)}
                         className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
                   >
                         <div className="grid grid-cols-1 space-y-3">
@@ -18,6 +23,9 @@ const LoginViews = () => {
                                     type="text"
                                     placeholder="Tu Nombre"
                                     className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                                    {...register('name', {
+                                          required: "El nombre es obligatorio"
+                                    })}
                               />
                         </div>
                         <div className="grid grid-cols-1 space-y-3">
@@ -27,6 +35,9 @@ const LoginViews = () => {
                                     type="email"
                                     placeholder="Email de Registro"
                                     className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                                    {...register('email', {
+                                          required: "El email es obligatorio"
+                                    })}
                               />
                         </div>
                         <div className="grid grid-cols-1 space-y-3">
@@ -36,6 +47,9 @@ const LoginViews = () => {
                                     type="text"
                                     placeholder="Nombre de usuario: sin espacios"
                                     className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                                    {...register('handle', {
+                                          required: "El handle es obligatorio"
+                                    })}
                               />
                         </div>
                         <div className="grid grid-cols-1 space-y-3">
@@ -43,18 +57,24 @@ const LoginViews = () => {
                               <input
                                     className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
                                     type="password"
-                                    id="passwor"
+                                    id="password"
                                     placeholder="Password de Registro"
+                                    {...register('password', {
+                                          required: "El password es obligatorio"
+                                    })}
                               />
                         </div>
 
                         <div className="grid grid-cols-1 space-y-3">
                               <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
                               <input
-                                    id="password"
+                                    id="password_confirmation"
                                     type="password"
                                     placeholder="Repetir Password"
                                     className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                                    {...register('password', {
+                                          required: "El password confirmación es obligatorio"
+                                    })}
                               />
                         </div>
 
