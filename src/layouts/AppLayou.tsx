8f10f11ec15d18b1c742481031acb3193db6f443
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-15 17:53:55 +0100

    adding src/api/Devtree and Using the useQuery Hook

--- src/layouts/AppLayou.tsx
+++ src/layouts/AppLayou.tsx
@@ -1,9 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import NavigationTabs from "../components/NavigationTabs";
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "../api/DevtreeApi";
+
 
 export default function AppLayout() {
 
+      const { data, isLoading, error, isError } = useQuery({
+            queryFn: getUser,
+            queryKey: ['user']
+      });
+
+      console.log(data);
 
       return (
             <>
