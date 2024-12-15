896fcc1e0c4678fa4f7c5f4838d756576772e1fd
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-15 20:13:43 +0100

    Getting user information and displaying it in the component

--- src/api/DevtreeApi.ts
+++ src/api/DevtreeApi.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from "axios";
 import api from "../config/axios";
+import { User } from "../types";
 
 
 export const getUser = async () => {
 
       try {
-            const { data } = await api('/user');
+            const { data } = await api<User>('/user');
             return data;
       } catch (error) {
             if (isAxiosError(error) && error.response) {
