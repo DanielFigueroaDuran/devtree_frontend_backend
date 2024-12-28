85434b99a1eb4dd057f36b0cd5d2ede90c34661a
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-28 11:53:11 +0100

    Adding the Social Network Array

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
+import { social } from "../data/social"
 
 
 const LinkTreeView = () => {
+      const [devtreeLinks, setDevtreeLinks] = useState(social);
+      console.log(devtreeLinks);
       return (
             <div>LinkTreeView</div>
       )
