edf5e7a95e898cc15f3b87dfc486281d6ac9134d
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-28 21:20:53 +0100

    Detecting if a URL is valid before enabling it

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { social } from "../data/social"
 import DetreeInput from "../components/DetreeInput";
-
+import { isValidUrl } from "../utlis";
+import { toast } from "sonner";
 
 const LinkTreeView = () => {
       const [devtreeLinks, setDevtreeLinks] = useState(social);
@@ -15,6 +16,21 @@ const LinkTreeView = () => {
             setDevtreeLinks(updatedLinks);
       };
 
+      const handleEnableLink = (socialNetwork: string) => {
+            const updatedLinks = devtreeLinks.map(link => {
+                  if (link.name === socialNetwork) {
+                        if (isValidUrl(link.url)) {
+                              return { ...link, enabled: !link.enabled };
+                        };
+                        toast.error('URL no valida');
+                  };
+                  return link;
+            });
+
+            // console.log(updatedLinks);
+            setDevtreeLinks(updatedLinks);
+      };
+
       return (
             <div className="space-y-5">
                   {devtreeLinks.map(item => (
@@ -22,6 +38,7 @@ const LinkTreeView = () => {
                               key={item.name}
                               item={item}
                               handleChange={handleChange}
+                              handleEnableLink={handleEnableLink}
                         />
                   ))}
             </div>
