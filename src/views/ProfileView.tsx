37d036a792e8abe4b66704d7e03bb9da4fd206e3
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-28 10:44:35 +0100

    Showing Profile Information and Image

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -35,6 +35,7 @@ export default function ProfileView() {
             },
             onSuccess: (data) => {
                   console.log(data);
+                  queryClient.invalidateQueries({ queryKey: ['user'] });
             }
       });
 
