2d28935075546737d3fcb28bdc323017c6878cd6
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-06 00:11:28 +0100

    Showing Handle, Image and Description

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,4 +1,5 @@
 import { UserHandle } from "../types"
+import { classNames } from "../utlis";
 
 type HandleDataProps = {
       data: UserHandle
@@ -6,7 +7,11 @@ type HandleDataProps = {
 
 const HandleData = ({ data }: HandleDataProps) => {
       return (
-            <div>HandleData</div>
+            <div className="space-y-6 text-white">
+                  <p className="text-5xl text-center font-black">{data.handle}</p>
+                  {data.image && < img src={data.image} className="max-w-[250px] mx-auto" />}
+                  <p className="text-lg text-center font-bold">{data.description}</p>
+            </div>
       )
 }
 
